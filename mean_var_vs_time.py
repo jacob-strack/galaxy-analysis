96abf52e6eaa8f6c67d513a61d4be0d918fc03ed
Jacob Strack <jstrack@fsu.edu>
2022-06-25 11:39:48 -0400

    Added script that plots an spatially averaged quantity over time

---
+++ mean_var_vs_time.py
@@ -0,0 +1,20 @@
+import yt
+import numpy as np 
+import matplotlib.pyplot as plt
+var = input("Plotting Variable:") #input a string which corresponds to a dataset variable 
+t_arr = [] 
+var_arr = [] 
+for i in range(0,120): 
+    filename ='DD' + str(i).zfill(4)
+    ds = yt.load(filename + '/' + filename)
+    ad = ds.all_data()
+    temp_var_arr = ad[var]
+    mean_var = np.mean(temp_var_arr)
+    t = ds.current_time
+    var_arr.append(mean_var)
+    t_arr.append(t)
+t_arr = np.array(t)
+var_arr = np.array(var_arr)
+plt.plot(t_arr, var_arr)
+plt.show() 
+
