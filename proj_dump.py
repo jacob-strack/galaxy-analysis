f65c7a472b0050af506cb63033706f1daafce751
jacob-strack <jstrack@fsu.edu>
2025-08-18 10:14:10 -0400

    added center functionality to proj_dump

--- proj_dump.py
+++ proj_dump.py
@@ -8,6 +8,15 @@ axis = 'z'
 for t in range(0,100): 
     try: 
         print(len(sys.argv))
+        if(len(sys.argv) == 4): 
+            field = sys.argv[1]
+            axis = sys.argv[2]
+            c = sys.argv[3]
+            print(axis)
+            ds = yt.load('DD' + str(t).zfill(4) + '/DD' + str(t).zfill(4))
+            proj = yt.ProjectionPlot(ds, axis, field, width = (200,"kpc"), weight_field = "ones", center=[c, c, c])
+            proj.annotate_timestamp(corner = "lower_right", redshift = False, draw_inset_box = True)
+            proj.save('frames/DD' + str(t).zfill(4) + '_Projection_' + axis + '_' + field + '.png')
         if(len(sys.argv) == 3): 
             field = sys.argv[1]
             axis = sys.argv[2]
